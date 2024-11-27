a84c76f1a6fe7e24fef4a2733ebd6125e52d72e5
Dineshk-2024 <dinesh42002@mail.com>
2024-11-27 19:34:30 +0530

    st

---
+++ SpringOAuth2Demo/src/main/java/com/example/SpringOAuth2Demo/HomeController.java
@@ -0,0 +1,14 @@
+package com.example.SpringOAuth2Demo;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @RequestMapping("/")
+    public String greet(){
+        return "welcome to OAuth2";
+    }
+}
